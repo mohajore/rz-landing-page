899132c3b4b40fb08672bd6d66078fedceb00ea0
mohammad aldokh <mohammad.aldokh@merwas.net>
2021-08-15 10:23:48 +0300

    first-comment-structure-the project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg'; 
+import './Assets/Style/main.scss'
+import { Component } from 'react';
+
+class App extends Component{
+  render(){
+    return(
+      <div id="app">
+
+
+
+
+
+      </div>
+    )}
+}
+
+export default App
